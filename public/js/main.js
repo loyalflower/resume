5d794fb56e073ee0aed7886c7d27decb6766c7ff
Neil Zeng <zwjzxh520@gmail.com>
2017-04-10 23:58:25 +0800

    init

---
+++ public/js/main.js
@@ -0,0 +1,32 @@
+"user strict";
+(function(){
+/*
+    var draw = {
+        "keyboard": function(node){
+
+        },
+
+        "voice": function(node){
+            var canvas = new fabric.Canvas(node);
+            console.log(canvas);
+            options = {
+                top: 0,
+                left: 0,
+                radius: 60,
+                fill: "green"
+            }
+            canvas.add(new fabric.Circle(options));
+        },
+ 
+        "emoji": function(node){
+
+        },
+
+        "plus": function(node){
+
+        }
+    };
+
+    draw.voice("voice");
+*/
+})();
